29d0e6e1ce907eef2481432eb76438951fdc2a15
mcanillas <maccgithub@gmail.com>
2025-11-25 19:01:23 +0800

    added data js and card in html

---
+++ data.js
@@ -0,0 +1,34 @@
+var initialDCars = [
+    {
+        model: "Toyota AE86",
+        img: "images/trueno86.jpg"
+    },
+    {
+        model: "Mazda RX-7 FD",
+        img: "images/rx-7fd.jpg"
+    },
+    {
+        model: "Mazda RX-7 FC",
+        img: "images/rx-7fc.jpg"
+    },
+    {
+        model: "Subaru Impreza WRX",
+        img: "images/subaru.jpg"
+    },
+    {
+        model: "Skyline GT-R R32",
+        img: "images/gtr-r32.jpg"
+    },
+    {
+        model: "Honda Civic EG6",
+        img: "images/eg6.jpg"
+    },
+    {
+        model: "Nissa Sileighty",
+        img: "images/sileighty.jpg"
+    },
+    {
+        model: "Lancer Evolution III",
+        img: "images/evo3.jpg"
+    },
+];

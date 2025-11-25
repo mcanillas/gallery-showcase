63706cf15cd4455bf20928e6e714bcf011763de3
mcanillas <maccgithub@gmail.com>
2025-11-25 19:27:49 +0800

    added click fuction

--- data.js
+++ data.js
@@ -1,34 +1,61 @@
 var initialDCars = [
-    {
-        model: "Toyota AE86",
-        img: "images/trueno86.jpg"
-    },
-    {
-        model: "Mazda RX-7 FD",
-        img: "images/rx-7fd.jpg"
-    },
-    {
-        model: "Mazda RX-7 FC",
-        img: "images/rx-7fc.jpg"
-    },
-    {
-        model: "Subaru Impreza WRX",
-        img: "images/subaru.jpg"
-    },
-    {
-        model: "Skyline GT-R R32",
-        img: "images/gtr-r32.jpg"
-    },
-    {
-        model: "Honda Civic EG6",
-        img: "images/eg6.jpg"
-    },
-    {
-        model: "Nissa Sileighty",
-        img: "images/sileighty.jpg"
-    },
-    {
-        model: "Lancer Evolution III",
-        img: "images/evo3.jpg"
-    },
+  {
+    model: "Toyota AE86",
+    img: "images/trueno86.jpg"
+  },
+  {
+    model: "Mazda RX-7 FD",
+    img: "images/rx-7fd.jpg"
+  },
+  {
+    model: "Mazda RX-7 FC",
+    img: "images/rx-7fc.jpg"
+  },
+  {
+    model: "Subaru Impreza WRX",
+    img: "images/subaru.jpg"
+  },
+  {
+    model: "Skyline GT-R R32",
+    img: "images/gtr-r32.jpg"
+  },
+  {
+    model: "Honda Civic EG6",
+    img: "images/eg6.jpg"
+  },
+  {
+    model: "Nissa Sileighty",
+    img: "images/sileighty.jpg"
+  },
+  {
+    model: "Lancer Evolution III",
+    img: "images/evo3.jpg"
+  },
+];
+
+var ownerCars = [
+  {
+    owner: "Owner: Takumi Fujiwara",
+  },
+  {
+    owner: "Owner: Keisuke Takahashi",
+  },
+  {
+    owner: "Owner: Ryosuke Takahashi",
+  },
+  {
+    owner: "Owner: Bunta Fujiwara",
+  },
+  {
+    owner: "Owner: Takeshi Nakazato",
+  },
+  {
+    owner: "Owner: Shingo Shoji",
+  },
+  {
+    owner: "Owner: Mako Sato",
+  },
+  {
+    owner: "Owner: Kyoichi Sudo",
+  },
 ];
